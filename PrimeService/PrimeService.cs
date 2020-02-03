103741d63c0e2eb6b4464557b32f680e2ffd9593
dj-hirrot <hirotoshibutani@gmail.com>
2020-02-03 22:37:46 +0900

    initrepo

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime.Service
+{
+    public class PrimeService
+    {
+      public bool IsPrime(int cadidate)
+      {
+        throw new NotImplemetedException("Not implemented.");
+      }
+    }
+}
